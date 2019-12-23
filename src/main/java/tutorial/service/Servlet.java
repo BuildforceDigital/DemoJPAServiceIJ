33b0a6f435d2f14202180e9a123b4305cf09ed9b
SeriousSoftware <vdenberg+gitkraken@gmail.com>
2019-12-23 16:42:27 +0100

    feat: javax.servlet-api to 4.0.1

--- src/main/java/tutorial/service/Servlet.java
+++ src/main/java/tutorial/service/Servlet.java
@@ -1,22 +1,19 @@
 package tutorial.service;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.sap.olingo.jpa.metadata.api.JPAEdmProvider;
+import com.sap.olingo.jpa.metadata.api.JPAEntityManagerFactory;
+import org.apache.olingo.commons.api.ex.ODataException;
+import org.apache.olingo.server.api.OData;
+import org.apache.olingo.server.api.ODataHttpHandler;
+import org.apache.olingo.server.api.ServiceMetadata;
 
 import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.olingo.commons.api.ex.ODataException;
-import com.sap.olingo.jpa.metadata.api.JPAEdmProvider;
-import com.sap.olingo.jpa.metadata.api.JPAEntityManagerFactory;
-import org.apache.olingo.server.api.OData;
-import org.apache.olingo.server.api.ODataHttpHandler;
-import org.apache.olingo.server.api.ServiceMetadata;
-import org.apache.olingo.commons.api.edmx.EdmxReference;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Servlet extends HttpServlet {
 
@@ -24,20 +21,20 @@ public class Servlet extends HttpServlet {
 	private static final String PUNIT_NAME = "DemoJPAServiceIJ";
 
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
 		try {
 
-			EntityManagerFactory emf = JPAEntityManagerFactory.getEntityManagerFactory(PUNIT_NAME, new HashMap<String, Object>());
+			EntityManagerFactory emf = JPAEntityManagerFactory.getEntityManagerFactory(PUNIT_NAME, new HashMap<>());
 			JPAEdmProvider metadataProvider = new JPAEdmProvider(PUNIT_NAME, emf, null, null);
 
 			OData odata = OData.newInstance();
-			ServiceMetadata edm = odata.createServiceMetadata(metadataProvider, new ArrayList<EdmxReference>());
+			ServiceMetadata edm = odata.createServiceMetadata(metadataProvider, new ArrayList<>());
 			ODataHttpHandler handler = odata.createHandler(edm);
 
 			handler.process(req, resp);
-		} catch (RuntimeException e) {
+		} catch (RuntimeException | ODataException e) {
 			throw new ServletException(e);
-		} catch (ODataException e) { throw new ServletException(e); }
-			 
+		}
+
 	}
 }
