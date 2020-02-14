9b363bfdd856b9bad324678e8615316d198590c2
SeriousSoftware <vdenberg+gitkraken@gmail.com>
2020-02-14 20:18:25 +0100

    feat:  Up to and including "3.2 ContextPath/ServletPath.svc in path.

--- src/main/java/tutorial/service/OdataServlet.java
+++ src/main/java/tutorial/service/OdataServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/DemoService.svc/*")
+@WebServlet(urlPatterns="/ServletPath.svc/*")
 public class OdataServlet extends HttpServlet {
 	protected static final String PUNIT_NAME = "TutorialPU";
 
