87bff12b4155405e2517d90db6ede22b14af81c1
SeriousSoftware <>
2020-09-20 17:37:43 +0200

    feat: Working order, no update function. Demo version 36.

--- src/main/resources/db/migration/V01__olingo.sql
+++ src/main/resources/db/migration/V01__olingo.sql
@@ -604,23 +604,6 @@ INSERT INTO OLINGO."Comment" VALUES ('1', 3, 'This is another test');
 
 --------GreenTrak --------------------------------------------------------------------------------------------------------
 
-create table OLINGO."Invoices" (
-    "ProductName"   varchar(36)    not null,
-    "Quantity"      varchar(36)    not null,
-    "ShipperName"   varchar(36)    not null,
-    "ExtendedPrice" DECIMAL(19, 4) not null,
-    "ShippingDate"  TIMESTAMP /* WITH TIME ZONE*/,
-    "Status"        varchar(1)     not null,
-
-    CONSTRAINT PK_Invoices PRIMARY KEY ("ProductName", "Quantity", "ShipperName")
-);
-
-INSERT INTO OLINGO."Invoices" VALUES ('Pineapple', 21, 'Fun Inc', 87.2000, '2015-04-01 12:00:00', 'A');
-INSERT INTO OLINGO."Invoices" VALUES ('Milk', 4, 'ACME', 9.99999,          '2015-02-18 00:00:00', 'B');
-INSERT INTO OLINGO."Invoices" VALUES ('Canned Beans', 3, 'ACME', 6.85000,  '2015-03-02 00:00:00', 'B');
-INSERT INTO OLINGO."Invoices" VALUES ('Salad', 2, 'ACME', 8.8000,          '2015-04-12 00:00:00', 'C');
-INSERT INTO OLINGO."Invoices" VALUES ('Bread', 1, 'Fun Inc', 2.71212,      '2015-01-27 00:00:00', 'A');
-
 create table OLINGO."AttendanceEventsAll" (
     "Id"               integer                  constraint attendanceeventsall_pk primary key,
     "ProjOwner"        varchar(36)              not null,
